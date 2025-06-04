dc931b81f62fdcc50128326ccc39389fc82655eb
Tomas Pastor Salazar <63889846+magic-tps@users.noreply.github.com>
2025-06-04 02:43:02 -0500

    Corrijo base para GitHub Page123s

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/HBMyLove/",
+  base: "/HB-Potona/",
   plugins: [react(), tailwindcss(),],
 })
